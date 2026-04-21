5f79b249d30007097420376d7c5bd51520d49849
Rafael Santos <rafael.santos.fina@gmail.com>
2026-04-21 19:17:07 -0300

    Aprendendo mais sobre condições

---
+++ exeoo8.js
@@ -0,0 +1,14 @@
+/* var velo = 90.5
+console.log(`A velocidade do seu veículo é ${velo}km/h`)
+if (velo > 60){
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirigja sempre usando o cinto de segurança!`) */
+
+var país = 'EUA'
+console.log(`Vivendo em um país lixo igual ao ${país}, é um`)
+if (país == 'Brasil'){
+    console.log('Brasileiro')
+} else {
+    console.log('País Ruim!')
+}
