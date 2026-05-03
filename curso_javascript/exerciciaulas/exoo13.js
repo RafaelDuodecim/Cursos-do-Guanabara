0694cb4b01ad3a64b7100be9ab97aa5f88d19417
Rafael Santos <rafael.santos.fina@gmail.com>
2026-05-03 16:37:02 -0300

    corrigindo formatação de psta

---
+++ curso_javascript/exerciciaulas/exoo13.js
@@ -0,0 +1,23 @@
+/* console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
+/*
+var c = 1 
+while (c <= 100) {
+    console.log(`Tudo bem? ${c}`)
+    c++
+}
+*/
+
+/* 
+var c = 1
+do { 
+  console.log(`Tudo bem? ${c}`)
+    c++
+} while (c <= 100)
+ */
+
---
+++ curso_javascript/exerciciaulas/script.js
@@ -0,0 +1,97 @@
+
+/*
+function carregar() {
+
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('foto')
+
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    var segundos = data.getSeconds()
+
+    // Formatar minutos e segundos
+    if (minutos < 10) {
+        minutos = '0' + minutos
+    }
+
+    if (segundos < 10) {
+        segundos = '0' + segundos
+    }
+
+    msg.innerHTML = `Agora são ${hora}:${minutos}:${segundos}`
+
+    // Troca de imagem
+    if (hora >= 0 && hora < 12) {
+        img.src = '../../fotos/manha.jpg'
+        document.body.style.background = '#87CEEB' // manhã
+    } else if (hora >= 12 && hora < 18) {
+        img.src = '../../fotos/tarde.jpg'
+        document.body.style.background = '#c9af81' // tarde
+    } else {
+        img.src = '../../fotos/noite.jpg'
+        document.body.style.background = '#031527' // noite
+    }
+}
+
+// Atualiza automaticamente
+window.onload = function() {
+    carregar()
+    setInterval(carregar, 1000)
+}
+
+*/
+
+
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+        var res = document.querySelector('#res')
+    var nascimento = Number(fano.value)
+    if (fano.value.length == 0 || nascimento > ano || nascimento < 0) {
+        window.alert('[Erro] Verifique seus dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - nascimento 
+        res.innerHTML = `Idade calculada: ${idade} anos`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10){
+                // Criança
+                img.setAttribute('src', 'crianca.png')
+            } else if  ( idade < 21) {
+                // Jovem 
+
+            } else if ( idade < 50) {
+                img.setAttribute('src', 'homemA.jpg')
+                // Adulto
+            } else {
+                // Idoso
+            }
+        } else if (fsex[0].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                // Criança
+            } else if  ( idade < 21) {
+                // Jovem 
+
+            } else if ( idade < 50) {
+                img.setAttribute('src', 'mulherA.jpg')
+               // Adulto
+            } else {
+                // Idoso
+            }
+        }
+
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
+
+//13:36
